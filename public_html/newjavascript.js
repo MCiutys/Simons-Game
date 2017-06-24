e1f848b6435365637622deafa0a42d6301d2c1a2
Mantas <Mantas@MantasPC>
2017-06-25 00:07:49 +0100

    - GetGuess method implemented (main loop changed).

--- public_html/newjavascript.js
+++ public_html/newjavascript.js
@@ -29,17 +29,39 @@
     // Function where all the logic is placed
     Simon.prototype.executeGame = function() {
         this.startGame();
-        do {
+        
+        
+        /*do {
             this.addOneColor();
             this.showSequence();
-            this.getGuess();
-        } while (this.isMatching());
+            var self = this;
+            this.getGuess(function() {
+                self.isMatching();
+            });
+        } while (this.isMatching());*/
+        
+        var self = this;
+        mainLoop();
+        
+        function mainLoop() {
+            if (self.isMatching()) {
+                self.addOneColor();
+                self.showSequence();
+                self.getGuess(function() {
+                    mainLoop();
+                });
+            } else {
+                console.log("Game is done");
+            }
+        }
+        
         this.stopGame();
     };
     
     // Function for starting the game
     Simon.prototype.startGame = function() {
         // Reset variables
+        console.log("---------------START GAME------------------");
         this.guess = "";
         this.currentSequence = "";
         this.result = 0;
@@ -47,6 +69,7 @@
     
     // Function to get one more color to make it harder for player
     Simon.prototype.addOneColor = function() {
+        console.log("---------------ADD ONE COLOUR------------------");
     // Getting one more color to guess
         var random = Math.floor(Math.random() * 4);
         switch(random) {
@@ -78,18 +101,30 @@
         x.style.backgroundColor = color;
     }
     
-    function pressColor() {
-        
-    }
+    Simon.prototype.pressGreen = function() {        
+        this.guess += GREEN;
+    };
+    
+    Simon.prototype.pressRed = function() {
+        this.guess += RED;
+    };
+    
+    Simon.prototype.pressBlue = function() {
+        this.guess += BLUE;
+    };
+    
+    Simon.prototype.pressYellow = function() {
+        this.guess += YELLOW;
+    };
 
     // Function to show flashing buttons
     Simon.prototype.showSequence = function() {
+        console.log("---------------SHOW SEQUENCE------------------");
         console.log(this.currentSequence);
         for (var i = 0; i < this.currentSequence.length; i++) {
             var place = "";
             var colorToSet = "";
             var colorToSetBack = "";
-            alert(place);
             if (this.currentSequence.charAt(i) === 'G') {
                 place = "quarter1";
                 colorToSet = LIGHT_GREEN;
@@ -107,27 +142,46 @@
                 colorToSet = LIGHT_YELLOW;
                 colorToSetBack = DARK_YELLOW;
             }
-            
             setColor(place, colorToSet);
             setTimeout(function() { setColorBack(place, colorToSetBack) }, 2000);
         }
     };
 
     // Function to get the guess from a user
-    Simon.prototype.getGuess = function() {
-        var counter = 0;
-        while (counter < this.currentSequence.length) {
-            
-        }
+    Simon.prototype.getGuess = function(callback) {
+        console.log("---------------GET GUESS------------------");
+        
+        var g = this;
+        
+        loop();
+       
+       function loop() {
+           console.log("loop");
+           console.log("Guess length: " + g.guess.length);
+           console.log("Current seq length: " + g.currentSequence.length);
+            if (g.guess.length < g.currentSequence.length) {
+                setTimeout(loop, 0);
+            } else {
+                console.log("callback");
+                callback();
+            }
+       }
     };
 
     // Check whether guess matches actual sequence
     Simon.prototype.isMatching = function() {
+        console.log("---------------IS MATCHING------------------");
         var match = false;
-        if (guess === currentSequence) {
-            result++;
+        console.log("Guess: " + this.guess);
+        console.log("Curr sequence: " + this.currentSequence);
+        if (this.guess === this.currentSequence) {
+            console.log("equal");
+            this.result++;
             match = true;
-        } 
+        } else {
+            console.log("not equal");
+        }
+        this.guess = "";
         return match;
     };
 
